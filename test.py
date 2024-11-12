8748d43c003faccb6b29d9a64f88237091a88ce9
MuntasirAraf <112501683+MuntasirAraf@users.noreply.github.com>
2024-11-12 23:23:17 +0600

    Variables inserted

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
 print('hello world!')
+print("My name is: Araf")
+print('x' * 10)
---
+++ variables.py
@@ -0,0 +1,13 @@
+'''price = 10
+rating = 5
+print(price * rating)
+name = input('what is your name? ')
+print('hi '+ name)'''
+
+name = input('what is your name? ')
+favourite_color = input('what is your favourite color? ')
+print(name + ' likes ' + favourite_color)
+
+birth_year = input('birth year: ')
+age = 2019 - int(birth_year)
+print(age)
