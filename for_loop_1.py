1aece406e83931b112c3f0460486751a5f0634ff
MuntasirAraf <112501683+MuntasirAraf@users.noreply.github.com>
2024-11-15 00:32:07 +0600

    Loops

---
+++ for_loop_1.py
@@ -0,0 +1,15 @@
+for item in 'Python':
+    print(item)
+
+for names in ['araf', 'ghuh', 'ghuhhh']:
+    print(names)
+
+
+for numbers_1 in range(10):
+    print(numbers_1)
+
+for numbers_2 in range(5, 10):
+    print(numbers_2) 
+
+for numbers_3 in range(5, 10, 2):
+    print(numbers_3)
