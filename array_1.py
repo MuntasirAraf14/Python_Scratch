ce4872bbf7772951fe6cffcda382a3641ee8c572
MuntasirAraf <112501683+MuntasirAraf@users.noreply.github.com>
2024-11-15 20:20:36 +0600

    arrays

---
+++ array_1.py
@@ -0,0 +1,6 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.insert(0, 10)
+numbers.remove(5)
+numbers.pop()
+numbers.sort()
+print(numbers) 
---
+++ remove_duplicates.py
@@ -0,0 +1,6 @@
+numbers = [2, 2, 4, 6, 3, 4, 6, 1]
+uniques = []
+for number in uniques:
+    if number not in uniques:
+        uniques.append(number)
+    print(uniques)
